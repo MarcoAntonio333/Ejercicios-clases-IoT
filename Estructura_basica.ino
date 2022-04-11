06d75a3aa9cf6bdbe764872a53e9924d39cd08c3
MarcoAntonio333 <96088823+MarcoAntonio333@users.noreply.github.com>
2022-04-11 16:56:25 -0500

    Create Estructura_basica.ino

---
+++ Estructura_basica.ino
@@ -0,0 +1,22 @@
+/*
+ *Estructura básica
+ *Marco Antonio
+ *11 abril 2022
+*/
+
+//bibliotecas
+//constantes
+//variables
+//definición de objetos
+
+//condiciones iniciales
+void setup (){
+//código aquí
+}
+
+//cuerpo de programa
+void loop(){
+//código principal aquí
+}
+
+//funciones de usuario
